b51947e8885bd11328431b6840de9ba91a556d18
Moses <moses.junior@student.moringaschool.com>
2022-04-12 20:12:46 +0300

    addition of house-class and class folder

---
+++ src/app/class/house-class.spec.ts
@@ -0,0 +1,7 @@
+import { HouseClass } from './house-class';
+
+describe('HouseClass', () => {
+  it('should create an instance', () => {
+    expect(new HouseClass()).toBeTruthy();
+  });
+});
---
+++ src/app/class/house-class.ts
@@ -0,0 +1,15 @@
+export class HouseClass {
+    constructor(
+        public name:string,
+        public address:string,
+        public number_of_bedrooms:number,
+        public apartments_spaces:number,
+        public apartments_available:number,
+        public house_image:string,
+        public livingroom_image:string,
+        public kitchen_image:string,
+        public sanitaryroom_image:string,
+        public bathroom_image:string,
+        public bedroom_image:string,
+    ){}
+}
