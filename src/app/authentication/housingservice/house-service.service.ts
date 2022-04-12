b865bd6af5aec1dd4ee28ea90140a9eaba7fd8be
Moses <moses.junior@student.moringaschool.com>
2022-04-12 20:13:43 +0300

    addition of house service

--- src/app/authentication/housingservice/house-service.service.ts
+++ src/app/authentication/housingservice/house-service.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { AuthserviceService } from 'src/app/authentication/authservice/authservice.service'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HouseServiceService {
-
-  constructor() { }
+  house:any
+  constructor(private http:HttpClient, private route:Router,private snackbar:MatSnackBar,private authService:AuthserviceService) { }
+  
+  add_house(house:any){
+    let headers = new HttpHeaders({
+      'Authorization':`Token ${sessionStorage.getItem('token')}`
+    })
+    this.http.post(`${environment.BASE_URL}housing/new_house`,house.value,{'headers':headers}).subscribe(response=>{
+      this.snackbar.open(`New house ${house.value.name} added to your lot`,"Thank you",{duration:3000})
+      this.route.navigate(['admin-panel'])
+    },error =>{
+      this.snackbar.open(`There was a problem with house development.PLease enter correct house details`,"Alright",{duration:3000})
+      console.log(error)
+    })
+  }
+  
+  
 }
